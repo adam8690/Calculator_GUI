a9a94c6593e918e24f11148337979de3e1fcc42e
Adam Baxter <adam8690@hotmail.co.uk>
2017-06-07 16:59:28 +0100

    set up an event listener for each button click

---
+++ Test/CalculatorControllerTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Adam on 07/06/2017.
+ */
+public class CalculatorControllerTest {
+}
--- src/calculator/CalculatorController.java
+++ src/calculator/CalculatorController.java
@@ -8,13 +8,73 @@ public class CalculatorController {
     double secondNum;
     String operator;
 
-    public void handleClearClick(ActionEvent event){
+    public void handleClearClick(){
         System.out.println("clear clicked");
     }
 
-    public void handleOneClicked(){
-        this.firstNum = 1;
-        System.out.println("firstNum" + firstNum);
+    public void handleOneClick(){
+        System.out.println("one clicked");
     }
+
+    public void handleTwoClick(){
+        System.out.println("Two clicked");
+    }
+
+    public void handleThreeClick(){
+        System.out.println("Three clicked");
+    }
+
+    public void handleFourClick(){
+        System.out.println("Four clicked");
+    }
+
+    public void handleFiveClick(){
+        System.out.println("Five clicked");
+    }
+
+    public void handleSixClick(){
+        System.out.println("Six clicked");
+    }
+
+    public void handleSevenClick(){
+        System.out.println("Seven clicked");
+    }
+
+    public void handleEightClick(){
+        System.out.println("Eight clicked");
+    }
+
+    public void handleNineClick(){
+        System.out.println("Nine clicked");
+    }
+
+    public void handleZeroClick(){
+        System.out.println("Zero clicked");
+    }
+
+    public void handlePlusClick(){
+        System.out.println("Plus clicked");
+    }
+
+    public void handleMinusClick(){
+        System.out.println("minus clicked");
+    }
+
+    public void handleTimesClick(){
+        System.out.println("Times clicked");
+    }
+
+    public void handleDivideClick(){
+        System.out.println("divide clicked");
+    }
+
+    public void handleEqualsClick(){
+        System.out.println("Equals clicked");
+    }
+
+    public void handlePointClick(){
+        System.out.println("Point clicked");
+    }
+
 
 }
