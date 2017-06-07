e71a9eba5a525568ef2458a3d643f7e3b8bcb708
Adam Baxter <adam8690@hotmail.co.uk>
2017-06-07 16:49:46 +0100

    added event handlers for buttons being clicked which print to console.

--- src/calculator/CalculatorController.java
+++ src/calculator/CalculatorController.java
@@ -1,4 +1,20 @@
 package calculator;
 
+import javafx.event.ActionEvent;
+
 public class CalculatorController {
+
+    double firstNum;
+    double secondNum;
+    String operator;
+
+    public void handleClearClick(ActionEvent event){
+        System.out.println("clear clicked");
+    }
+
+    public void handleOneClicked(){
+        this.firstNum = 1;
+        System.out.println("firstNum" + firstNum);
+    }
+
 }
