80dfe2a749268d1079fdef4ff4d9a6452d64f920
Adam Baxter <adam8690@hotmail.co.uk>
2017-06-08 15:52:52 +0100

    calculator logic working

--- src/calculator/CalculatorController.java
+++ src/calculator/CalculatorController.java
@@ -3,22 +3,55 @@ package calculator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import static calculator.CalculatorView.*;
-
 public class CalculatorController {
 
-    double firstNum;
-    double secondNum;
+    Double firstNum;
+    Double secondNum;
+    Double result;
     String numberToShow = "";
-    String operator = "none";
+    String operator;
     Boolean decimal = false;
-    CalculatorView calculatorView;
+    CalculatorView calculatorView = new CalculatorView();
+    CalculatorModel calculatorModel = new CalculatorModel();
 
 //  as this class is specified in FXML file as the controller and the label showing the calculator display has an fxid of display, the variable display here
 //  knows it refers to the calculator display.
     @FXML
     Label display;
 
+    public void clearDisplay(){
+        this.numberToShow = "";
+        this.decimal = false;
+        this.result = null;
+        display.setText("0");
+    }
+
+    public void performOperation(){
+        if(this.firstNum == null){
+            this.firstNum = Double.parseDouble(display.getText());
+            display.setText("");
+            clearDisplay();
+        }
+        else {
+            this.secondNum = Double.parseDouble(display.getText());
+            clearDisplay();
+            switch (this.operator){
+                case "plus": this.calculatorModel.add(firstNum, secondNum);
+                break;
+                case "minus": this.calculatorModel.subtract(firstNum, secondNum);
+                break;
+                case "times": this.calculatorModel.multiply(firstNum, secondNum);
+                break;
+                case "divide": this.calculatorModel.divide(firstNum, secondNum);
+                break;
+            }
+
+            this.result = calculatorModel.getResult();
+            display.setText(this.result.toString());
+            this.firstNum = this.result;
+            this.secondNum = null;
+        }
+    }
 
     public void handleOneClick(){
         this.numberToShow += "1";
@@ -73,7 +106,7 @@ public class CalculatorController {
     }
 
     public void handlePointClick(){
-        if(this.numberToShow.equals("") && !this.decimal){
+        if(!this.decimal && this.numberToShow.equals("") ){
             this.numberToShow = "0.";
             display.setText(this.numberToShow);
             this.decimal = true;
@@ -87,38 +120,39 @@ public class CalculatorController {
     }
 
     public void handleClearClick(){
-        this.numberToShow = "";
-        this.decimal = false;
-        display.setText("0");
+        clearDisplay();
+        this.firstNum = null;
+        this.secondNum = null;
+        this.result = null;
     }
 
-    //    TODO handle operator behaviour
-
     public void handlePlusClick(){
-        System.out.println("Plus clicked");
+        this.operator = "plus";
+        performOperation();
     }
 
     public void handleMinusClick(){
-        System.out.println("minus clicked");
+        this.operator = "minus";
+        performOperation();
     }
 
     public void handleTimesClick(){
-        System.out.println("times clicked");
+        this.operator = "times";
+        performOperation();
     }
 
     public void handleDivideClick(){
-        System.out.println("divide clicked");
+        this.operator = "divide";
+        performOperation();
     }
-
-
-
-//TODO handle equals behaviour
 
     public void handleEqualsClick(){
-        System.out.println("Equals clicked");
+        if(this.operator != null) {
+            performOperation();
+            this.firstNum = null;
+            this.secondNum = null;
+            this.operator = null;
+            display.setText(this.result.toString());
+        }
     }
-
-
-
-
 }
